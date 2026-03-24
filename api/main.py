ed0b3daf4e0b9dd605549be0233b1944668c42a4
edgary1701 <edgar.yupanqui+pacifico@bluetab.net>
2026-03-24 15:05:43 -0500

    subir archivos

--- api/main.py
+++ api/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "API running"}
+    return {"test": "ejecutando FastAPI...."}
+
+@app.post("/upload/{table}")
+async def upload_file(table: str, file: UploadFile = File(...)):
+    
+    file_location = f"data/{table}_{file.filename}"
+    
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+
+    return {
+        "": f"Archivo cargado for {table}",
+        "Ruta": file_location
+    }
